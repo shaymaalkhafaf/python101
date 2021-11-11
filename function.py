22740ec582ac553bce78f7cb9c5372f3240e7aac
Shayma Alkhafaf <79552804+shaymaalkhafaf@users.noreply.github.com>
2021-11-11 15:16:33 -0600

    Add files via upload

---
+++ function.py
@@ -0,0 +1,6 @@
+noun= input("enter noun?") 
+name= input("enter the name?")
+verb1= input("enter verb 1")
+verb2= input("enter verb2")
+
+print("There once was a" + noun + " " + "that suddenly came to life," + name + " " + "and starting" + verb1 + "around the town." + name + "decided that" + verb2 + "was unacceptable and decided to move to the countryside an live a life of solitude.")
---
+++ tip_calculator.py
@@ -0,0 +1,18 @@
+
+
+
+bill_total= float(input(" what is the total bill?"))
+percentage= float(input(" what percentage would you like to tip? "))
+
+def percentageplus(percentage, bill_total):
+    tip=((bill_total * percentage) / 100)
+    total=(bill_total + tip)
+    return total
+
+
+
+
+
+
+
+    
